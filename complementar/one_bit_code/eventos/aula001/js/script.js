d6ac5110b374d4c09a51b49c94f0b9b349e90ccf
isaacgoncod <isaacgoncod@gmail.com>
2023-02-07 22:26:17 -0300

    one_bit

---
+++ complementar/one_bit_code/eventos/aula001/js/script.js
@@ -0,0 +1,15 @@
+function clickMe() {
+  console.log("Fui clicado");
+}
+function focusMe() {
+  console.log("Fui Focado");
+} 
+function blurMe() {
+  console.log("Fui Desfocado");
+}
+function changeMe() {
+  console.log("Fui Alerado");
+}
+function overMe() {
+  console.log("Você passou por cima");
+}

9e49d9c3ade2e75f3385f9541ba275dc24085429
isaacgoncod <isaacgoncod@gmail.com>
2023-03-09 23:06:19 -0300

    Tarefa

--- senai/aula010/api/controllers/calculadora.controller.js
+++ senai/aula010/api/controllers/calculadora.controller.js
@@ -1,11 +1,36 @@
 const somar = (req, res) => {
+  const { operacao } = req.params;
   const { vali, valii } = req.query;
 
   let resultado = Number(vali) + Number(valii);
 
   res.status(200).json({ resultado: resultado }).end();
 };
+const subtrair = (req, res) => {
+  const { vali, valii } = req.query;
+
+  let resultado = Number(vali) - Number(valii);
+
+  res.status(200).json({ resultado: resultado }).end();
+};
+const multiplicar = (req, res) => {
+  const { vali, valii } = req.query;
+
+  let resultado = Number(vali) * Number(valii);
+
+  res.status(200).json({ resultado: resultado }).end();
+};
+const dividir = (req, res) => {
+  const { vali, valii } = req.query;
+
+  let resultado = Number(vali) / Number(valii);
+
+  res.status(200).json({ resultado: resultado }).end();
+};
 
 module.exports = {
   somar,
+  subtrair,
+  multiplicar,
+  dividir,
 };
--- senai/aula010/api/routes/calculadora.routes.js
+++ senai/aula010/api/routes/calculadora.routes.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const CalculadoraController = require("../controllers/calculadora.controller");
 
 router.get("/calcular/:operacao", CalculadoraController.somar);
+router.get("/calcular/:operacao", CalculadoraController.subtrair);
+router.get("/calcular/:operacao", CalculadoraController.multiplicar);
+router.get("/calcular/:operacao", CalculadoraController.dividir);
 
 module.exports = router;
